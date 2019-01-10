f2ca4729ded632cf0d2745e53a600eb095119eaa
Richardwoo999 <2672205497@qq.com>
2019-01-10 20:26:28 +0800

    SpringCload项目002

--- src/main/java/mssc/applet/impl/Application15.java
+++ src/main/java/mssc/applet/impl/Application15.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Application05 {
+public class Application15 {
     //Test
     public static void main(String[] args) {
-        SpringApplication.run(Application05.class, args);
+        SpringApplication.run(Application15.class, args);
     }
 
 }
