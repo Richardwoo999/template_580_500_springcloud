9e5233c36b895f6f61fb3ed6234f378397d83b7c
Richardwoo999 <2672205497@qq.com>
2019-01-10 20:06:22 +0800

    SpringCload项目001

---
+++ src/main/java/mssc/applet/impl/Application05.java
@@ -0,0 +1,14 @@
+package mssc.applet.impl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application05 {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application05.class, args);
+    }
+
+}
+
