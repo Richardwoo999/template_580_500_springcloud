506de83aa318a93ed0a8059bc18e2e47fe62b1cc
Richardwoo999 <2672205497@qq.com>
2019-01-10 18:46:02 +0800

    SpringCload项目

---
+++ src/test/java/template_580_500_springcloud/template_580_500_springcloud/Template580500SpringcloudApplicationTests.java
@@ -0,0 +1,17 @@
+package template_580_500_springcloud.template_580_500_springcloud;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Template580500SpringcloudApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
+
